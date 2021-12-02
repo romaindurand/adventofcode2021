6f2cffab7fc80a191e77f8b1c9183f14c8f7e92f
Romain Durand <romain.durand@radiofrance.com>
2021-12-02 23:31:48 +0100

    day1 part2

--- 01/index.js
+++ 01/index.js
@@ -2,10 +2,25 @@ import { readFile } from "../utils.js";
 
 (async () => {
   const file = await readFile("01/input.txt");
-  const result = file.reduce((memo, line, index) => {
+  const resultPart1 = file.reduce((memo, line, index) => {
     if (index === 0) return memo;
     if (Number(line) > Number(file[index - 1])) memo++;
     return memo;
   }, 0);
-  console.log({ result });
+  console.log({ resultPart1 });
+
+  const resultPart2 = file.reduce((memo, line, index) => {
+    if (index < 3) return memo;
+    const windowSum = getWindowSum(file, index);
+    const previousWindowSum = getWindowSum(file, index - 1);
+    if (windowSum > previousWindowSum) memo++;
+    return memo;
+  }, 0);
+  console.log({ resultPart2 });
 })();
+
+function getWindowSum(file, index) {
+  return (
+    Number(file[index - 2]) + Number(file[index - 1]) + Number(file[index])
+  );
+}
