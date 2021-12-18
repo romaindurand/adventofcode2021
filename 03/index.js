f3502820dad86a23bd9e949a26e0a89b98235853
romaindurand <romaindurand89@gmail.com>
2021-12-18 19:14:13 +0100

    wip

--- 03/index.js
+++ 03/index.js
@@ -1,26 +1,42 @@
 import { readFile } from "../utils.js";
 
 (async () => {
-  const file = await readFile("03/input.txt")
-  const {gammaRate, epsilonRate} = computeRates(file)
-  const powerConsumption = gammaRate * epsilonRate
-  console.log({powerConsumption, gammaRate, epsilonRate})
-})()
+  const file = await readFile("03/input.txt");
+  const { gammaRate, epsilonRate, binaryEpsilonRate, binaryGammaRate } =
+    computeRates(file);
+  const powerConsumption = gammaRate * epsilonRate;
+  console.log({ powerConsumption, gammaRate, epsilonRate });
+
+  computeGasRatings(binaryEpsilonRate, file);
+})();
+
+function computeGasRatings(bitCriteria, file) {
+  let lines = [...file];
+  let bitIndex = 0;
+  while (lines.length > 1) {
+    lines = lines.filter((line) => line[bitIndex] === bitCriteria[bitIndex]);
+    bitIndex++;
+  }
+  console.log({ bitCriteria, lines });
+}
 
 function computeRates(lines) {
-  const bit1Count = Array(lines[0].length).fill(0)
-  lines.forEach(line => {
-    line.split('').forEach((char, index) => {
-      if (Number(char) === 1) bit1Count[index]++
-    })
-  })
+  const bit1Count = Array(lines[0].length).fill(0);
+  lines.forEach((line) => {
+    line.split("").forEach((char, index) => {
+      if (Number(char) === 1) bit1Count[index]++;
+    });
+  });
 
   const binaryGammaRate = bit1Count.reduce((memo, count) => {
-    memo += count > lines.length/2 ? 1 : 0
-    return memo
-  }, "")
-  const gammaRate = Number.parseInt(binaryGammaRate, 2)
-  const binaryEpsilonRate = binaryGammaRate.split('').map(char => char === '1' ? '0' : '1').join('')
-  const epsilonRate = Number.parseInt(binaryEpsilonRate, 2)
-  return {gammaRate, epsilonRate}
+    memo += count > lines.length / 2 ? 1 : 0;
+    return memo;
+  }, "");
+  const gammaRate = Number.parseInt(binaryGammaRate, 2);
+  const binaryEpsilonRate = binaryGammaRate
+    .split("")
+    .map((char) => (char === "1" ? "0" : "1"))
+    .join("");
+  const epsilonRate = Number.parseInt(binaryEpsilonRate, 2);
+  return { gammaRate, epsilonRate, binaryEpsilonRate, binaryGammaRate };
 }
